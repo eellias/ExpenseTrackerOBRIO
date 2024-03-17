d62a4aee422b2f5646342f6b94cd05beed0f4c04
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2024-03-17 14:56:18 +0200

    Removed storyboard.

---
+++ ExpenseTrackerOBRIO/ExpenseTrackerOBRIO/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExpenseTrackerOBRIO
+//
+//  Created by eellias on 17.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
