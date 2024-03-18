231ffb41ba8556ce88fea951e5dc3cef53e89f3f
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2024-03-18 11:36:56 +0200

    Added Bitcoin currency fetching functionality.

---
+++ ExpenseTrackerOBRIO/ExpenseTrackerOBRIO/Models/BitcoinCurrency.swift
@@ -0,0 +1,31 @@
+//
+//  BitcoinCurrency.swift
+//  ExpenseTrackerOBRIO
+//
+//  Created by eellias on 18.03.2024.
+//
+
+import Foundation
+
+struct BitcoinCurrency: Codable {
+    let time: Time
+    let bpi: Bpi
+}
+
+struct Time: Codable {
+    let updated: String
+}
+
+struct Bpi: Codable {
+    let usd: USD
+
+    enum CodingKeys: String, CodingKey {
+        case usd = "USD"
+    }
+}
+
+struct USD: Codable {
+    let symbol: String
+    let rate: String
+    let rate_float: Double
+}
